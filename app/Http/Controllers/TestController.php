992812e64ea52b0db4bd4d2371b297f14be2f876
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-14 22:08:03 +0900

    Laravel環境構築まで完了

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Test;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        $values =Test::all();
+
+        $tests = DB::table('tests')->select('id')->get();
+        dd($tests);
+        return view('tests.test', compact('values'));
+    }
+}
