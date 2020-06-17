cc2c919bab559df2ea1f232e027bbeb905537c05
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-17 21:33:48 +0900

    新規登録機能実装

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -15,7 +15,7 @@
                     @endif
 
                     createです。
-                    <form action="" method="POST">
+                    <form action="{{ route('contact.store') }}" method="POST">
                         @csrf
                         氏名
                         <input type="text" name="your_name">
