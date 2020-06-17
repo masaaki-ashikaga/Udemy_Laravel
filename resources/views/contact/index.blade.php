091566267d25c94516c890b132f8d728219b0241
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-17 21:51:30 +0900

    一覧表示機能実装

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -13,8 +13,32 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    {{-- <a href="{{ route('contact.create') }}">新規登録</a> --}}
+                    <form action="{{ route('contact.create') }}" method="GET">
+                        @csrf
+                        <button type="submit" class="btn btn-primary">新規登録</button>
+                    </form>
                     indexです。
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th>ID</th>
+                            <th scope="col">氏名</th>
+                            <th scope="col">タイトル</th>
+                            <th scope="col">登録日時</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($contacts as $contact)
+                          <tr>
+                            <th scope="row">{{ $contact->id }}</th>
+                            <td>{{ $contact->your_name }}</td>
+                            <td>{{ $contact->title }}</td>
+                            <td>{{ $contact->created_at }}</td>
+                          </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
